0c5df9e4f46233b71f86021a5c659bec54ce14ed
Natalia Grinevich <nata.grine@gmail.com>
2016-11-29 15:59:52 +0200

    Add carthage support

---
+++ RangeSlider/RangeSlider.h
@@ -0,0 +1,19 @@
+//
+//  RangeSlider.h
+//  RangeSlider
+//
+//  Created by Natalia Grinevich on 29.11.16.
+//  Copyright © 2016 Null Monkey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RangeSlider.
+FOUNDATION_EXPORT double RangeSliderVersionNumber;
+
+//! Project version string for RangeSlider.
+FOUNDATION_EXPORT const unsigned char RangeSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RangeSlider/PublicHeader.h>
+
+
